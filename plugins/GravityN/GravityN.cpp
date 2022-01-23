998a37c25019a66e538eba5e99837db8c52c8dbf
aleksandarkoruga <aleksandar.koruga@gmail.com>
2022-01-23 16:13:51 +0100

    Helpflile updated

--- plugins/GravityN/GravityN.cpp
+++ plugins/GravityN/GravityN.cpp
@@ -5,25 +5,109 @@
 #include "GravityN.hpp"
 
 static InterfaceTable* ft;
-
 namespace GravityN {
 
-GravityN::GravityN() {
+GravityN::GravityN()  :parameters() {
+    InitializeComponents(&system, this);
     mCalcFunc = make_calc_function<GravityN, &GravityN::next>();
+
+    mnMasses=(mNumInputs - 4);
+    system1=GSystem(mnMasses);
+    system2=GSystem(mnMasses);
+    system=GSysPAir(mnMasses);
+    for (auto i = 0; i < mnMasses; PZ(i))
+    {
+        system[i] = std::make_pair(&system1[i], &system2[i]);
+    }
+    
     next(1);
 }
 
 void GravityN::next(int nSamples) {
-    const float* input = in(0);
-    const float* gain = in(1);
-    float* outbuf = out(0);
+    
 
-    // simple gain function
-    for (int i = 0; i < nSamples; ++i) {
-        outbuf[i] = input[i] * gain[i];
+    for (int sCnt = 0; sCnt < nSamples; PZ(sCnt))
+    {
+        for (int i = 0; i < mnMasses; PZ(i))
+            system[i].first->mass = in(i)[sCnt];
+        
+        parameters.SetGravity    ((in(mnMasses)[sCnt]));
+        SetTimestep              ((in(mnMasses +1)[sCnt]));
+        parameters.SetAttenuation((in(mnMasses +2)[sCnt]));
+        parameters.SetMaxDistance((in(mnMasses + 3)[sCnt]));
+
+        UpdateSystem(&system, &parameters);
+
+        for (int i = 0; i < mnMasses; PZ(i))
+        {
+            //output the current state
+            out(i * 2)[sCnt] = system[i].first->p.x;
+            out(i * 2 + 1)[sCnt] = system[i].first->p.y;
+        }
     }
 }
 
+void UpdateSystem(GSysPAir* systems, GParams* params)
+{
+
+    
+
+    for (auto currentPair : *systems)
+    {
+        glm::vec2 forces(0);
+        auto* current = currentPair.first;
+
+        if ( std::abs(current->mass)< 1e-9)
+        {
+            continue;
+        }
+            for (auto otherPair : *systems)
+            {
+
+                auto* other = otherPair.first;
+
+                auto dist = glm::distance(current->p, other->p);
+                if (dist > MAX_PROXIMITY)
+                {
+                    auto geomPart = glm::normalize(other->p - current->p) / std::powf(dist, 2);
+                    forces += (params->GetGravity() * current->mass * other->mass * geomPart);
+                }
+                else if (current != other) {
+                    //collision
+                    current->p = glm::vec2(((std::rand() / (float)RAND_MAX) - 0.5) * params->GetMaxDistance(), ((std::rand() / (float)RAND_MAX) - 0.5) * params->GetMaxDistance());
+                    current->v = glm::vec2(0, 0);
+                }
+            }
+
+        //attenuate in opposite direction of velocity;
+        if (glm::length(forces) > 0 && glm::length(current->v) > 0)
+            forces += (-glm::normalize(current->v) * params->GetAttenuation());
+        auto* destination = currentPair.second;
+        auto dT = params->GetDt();
+        auto acceleration = forces / current->mass;
+
+        destination->v = current->v + acceleration * dT;
+        destination->p += destination->v * dT;
+        if (glm::length(destination->p) > params->GetMaxDistance())
+        {
+            
+            destination->p = glm::vec2(((std::rand() / (float)RAND_MAX) - 0.5) * params->GetMaxDistance(), ((std::rand() / (float)RAND_MAX )- 0.5) * params->GetMaxDistance());
+            destination->v = glm::vec2(0, 0);
+            //reverse and half speed vector
+               // destination->v = glm::vec2(0, 0);//-glm::normalize(destination->v) * glm::length(destination->v);
+               // destination->p = glm::vec2((std::rand() / (float)RAND_MAX - 0.5) * 2, (std::rand() / (float)RAND_MAX - 0.5) * 2);//destination->v * dT;
+
+        }
+    
+    }
+
+    //swap the pointers
+    for (auto &component : *systems)
+    {
+        std::swap(component.first, component.second);
+    }
+   
+}
 } // namespace GravityN
 
 PluginLoad(GravityNUGens) {
--- plugins/GravityN/GravityN.hpp
+++ plugins/GravityN/GravityN.hpp
@@ -1,11 +1,72 @@
 // PluginGravityN.hpp
 // Aleksandar Koruga (aleksandar.koruga@gmail.com)
 
+//
+//
 #pragma once
 
 #include "SC_PlugIn.hpp"
+#include <vector>
+#include <utility>
+#include "GLM 0.9.9.8/g-truc-glm-bf71a83/glm/glm.hpp"
+
 
 namespace GravityN {
+    
+#define MAX_PROXIMITY 1.0f
+#define DEFAULT_TIMESTEP 0.0000226757369615f
+
+    struct MassComponent 
+    {
+        float mass;        
+        glm::vec2 p;
+        glm::vec2 v;
+        MassComponent() : mass(1), p(0), v(0) {}
+        inline bool operator==(const MassComponent& rhs) { return glm::distance(this->p, rhs.p) < MAX_PROXIMITY; };
+        inline bool operator!=(const MassComponent& rhs) { return !( *this == rhs); };
+
+    };
+    
+    using GSystem = std::vector<MassComponent> ;
+    using GSysPAir = std::vector<std::pair<MassComponent*, MassComponent*>>;
+
+    struct GParams 
+    {
+    protected: glm::vec4 params;
+    public:
+        GParams(): params(glm::vec4(1e-6f, DEFAULT_TIMESTEP,1000.0f,0.0f)) {}
+        float GetGravity(){ return params.x; };
+        float GetDt()       { return params.y; };
+        float GetMaxDistance() { return params.z; };
+        float GetAttenuation() { return params.w; };
+
+        void SetGravity( float g) {  params.x=g; };
+        void SetDt(float dt) {  params.y=dt; };
+        void SetMaxDistance(float d) { params.z=d; };
+        void SetAttenuation(float a) { params.w = a; };
+    };
+        
+    
+    float GetRandom(SCUnit* unit) {
+        return unit->mParent->mRGen->frand2();
+    }
+
+    void InitializeComponents(GSysPAir* _system, SCUnit* unit)
+    {
+        for (auto component : *_system) 
+        {
+            auto x = GetRandom(unit)*100.0f;
+            auto y = GetRandom(unit)*100.0f;
+            component.first->p.x = x;
+            component.second->p.x = x;
+            
+            component.first->p.y = y;
+            component.second->p.y = y;
+        }
+    };  
+    void UpdateSystem(GSysPAir* systems,GParams* params);
+
+
 
 class GravityN : public SCUnit {
 public:
@@ -19,6 +80,14 @@ private:
     void next(int nSamples);
 
     // Member variables
+protected:
+    GSystem system1;
+    GSystem system2;
+    GSysPAir system;
+    int mnMasses;
+    // gravity dt distance
+    GParams parameters;
+    inline void SetTimestep(float dT) { parameters.SetDt( (float)mRate->mSampleDur* dT ); }
 };
 
 } // namespace GravityN
